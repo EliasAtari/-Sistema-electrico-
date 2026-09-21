a4da6fb634e18923076ca3b37cdcf05644b37fe9
Elías-de-Programacion <eliascanales110@gmail.com>
2026-09-20 22:02:58 -0500

    Update main.js

--- behavior_pack/scripts/main.js
+++ behavior_pack/scripts/main.js
@@ -1,1 +1,36 @@
+import { world } from "@minecraft/server";
 
+world.afterEvents.itemUse.subscribe((evento) => {
+    const jugador = evento.source;
+    const item = evento.itemStack;
+
+    // Solo el cable de aluminio
+    if (item.typeId !== "electricidad:cable_aluminio") {
+        return;
+    }
+
+    // Busca el bloque que está mirando el jugador
+    const objetivo = jugador.getBlockFromViewDirection({
+        maxDistance: 6
+    });
+
+    if (!objetivo) {
+        return;
+    }
+
+    const bloque = objetivo.block;
+    const cara = objetivo.face;
+
+    // Posición donde aparecerá el cable
+    const ubicacion = {
+        x: bloque.location.x + 0.5,
+        y: bloque.location.y + 1.05,
+        z: bloque.location.z + 0.5
+    };
+
+    // Crear el cable de aluminio
+    jugador.dimension.spawnEntity(
+        "electricidad:cable_electrico",
+        ubicacion
+    );
+});
